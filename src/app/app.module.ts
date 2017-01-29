5ca1faa9a53ef23a88cbc6616a2c25648762a2d3
MatthieuBoxho <matthieu.boxho@gmail.com>
2017-01-29 14:26:34 +0000

    Added GameComponent and its Route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
